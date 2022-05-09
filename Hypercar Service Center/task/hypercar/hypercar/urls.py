219c8220b8b85293b11daf1572238fe7948e1d04
Marcin Majewski <martinpl@gmail.com>
2022-05-09 12:46:20 +0200

    After stage 3 (creating tickets from /menu/ page and adding them to deque)

--- Hypercar Service Center/task/hypercar/hypercar/urls.py
+++ Hypercar Service Center/task/hypercar/hypercar/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from tickets.views import WelcomeView, MenuView
+from tickets.views import WelcomeView, MenuView, TicketView
 
 
 urlpatterns = [
     path('welcome/', WelcomeView.as_view()),
     path('menu/', MenuView.as_view()),
+    path('get_ticket/<ticket_type>', TicketView.as_view()),
 ]
--- Hypercar Service Center/task/hypercar/tickets/views.py
+++ Hypercar Service Center/task/hypercar/tickets/views.py
@@ -1,6 +1,9 @@
 from django.views import View
 from django.http.response import HttpResponse
 from django.shortcuts import render
+from collections import deque
+
+ticket_nr = 0
 
 
 class WelcomeView(View):
@@ -11,3 +14,33 @@ class WelcomeView(View):
 class MenuView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'tickets/menu.html')
+
+
+class TicketView(View):
+    # queues for different services
+    change_oil = deque()
+    inflate_tires = deque()
+    diagnostic = deque()
+    full_queue = {'change_oil': change_oil,
+                  'inflate_tires': inflate_tires,
+                  'diagnostic': diagnostic}
+
+    def get(self, request, ticket_type):
+        # global full_queue
+        global ticket_nr
+
+        ticket_nr += 1
+        wait_change_oil = 2 * len(self.full_queue['change_oil'])
+        wait_inflate_tires = 5 * len(self.full_queue['inflate_tires'])
+        wait_diagnostics = 30 * len(self.full_queue['diagnostic'])
+        wait_time = 0
+        if ticket_type == 'change_oil':
+            wait_time = wait_change_oil
+        elif ticket_type == 'inflate_tires':
+            wait_time = wait_inflate_tires + wait_change_oil
+        elif ticket_type == 'diagnostic':
+            wait_time = wait_diagnostics + wait_inflate_tires + wait_change_oil
+        self.full_queue[ticket_type].append(ticket_nr)
+        return render(request, 'tickets/ticket.html',
+                      {'ticket_nr': ticket_nr,
+                       'wait_time': wait_time})
